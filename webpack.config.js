634b6f52effe619cf510dbbd50c0d93b275530ce
toby <toby@canva.com>
2017-01-23 15:11:17 +1100

    entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-	entry: { 'events': 'events' },
+	context: __dirname,
+	entry: './src/entry.js',
 	resolve: {
-		modules: [__dirname + '/src', 'node_modules']
+		modules: [__dirname + '/src', 'node_modules'],
+		extensions: ['js']
 	},
 	output: {
 		filename: 'out.js'
